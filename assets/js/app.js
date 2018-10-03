714409c2b0f73b45794c475b1e113c409c7c3b95
andresnew00 <andresnew00@hotmail.com>
2018-10-02 23:47:23 -0400

    horrible commit #Ihatefirebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCOWwoh5G0W6SG7Gel0KkfAwutzko-Xbok",
+    authDomain: "homework-train-dc329.firebaseapp.com",
+    databaseURL: "https://homework-train-dc329.firebaseio.com",
+    projectId: "homework-train-dc329",
+    storageBucket: "homework-train-dc329.appspot.com",
+    messagingSenderId: "877537434101"
+  };
+  firebase.initializeApp(config);
